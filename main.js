022bc0c760610fc9ae2074df22817e55fb90f2cf
kyuveloper <jinkyuk127@gmail.com>
2023-10-18 09:35:57 +0900

    resume

--- main.js
+++ main.js
@@ -19,3 +19,34 @@ window.addEventListener('resize', () => {
         toggleMenu.classList.remove('active');
     }
 })
+
+
+// Main Cover Twinkle Text
+let target = document.querySelector('#dynamic');
+
+function randomString() {
+    let stringList = ["매우 꼼꼼한 성격", "끈기있게 해결하는 습관", "협업을 이끄는 능력"];
+    let selectString = stringList[Math.floor(Math.random() * stringList.length)];
+    let selectedStringAl = selectString.split("");
+    return selectedStringAl;
+}
+function resetTyping() {
+    target.textContent = "";
+    dynamic(randomString());
+}
+function dynamic(randomList) {
+    if (randomList.length > 0) {
+        target.textContent += randomList.shift();
+        setTimeout(function() {
+            dynamic(randomList);
+        }, 80);
+    } else {
+        setTimeout(resetTyping, 2000);
+    }
+}
+dynamic(randomString());
+function twinkle() {
+    target.classList.toggle("twinkle");
+}
+setInterval(twinkle, 500);
+
